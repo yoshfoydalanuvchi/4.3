a8da0708a919df39eba95e7808afe54d0a652563
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-06-05 19:16:40 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,156 @@
+// 1-misol
+
+// let a;
+
+// a = +prompt("Enter a value for a");
+// if (0 < a) {
+//   alert(Math.trunc(a + 1));
+// } else {
+//   alert(Math.trunc(a));
+// }
+
+// 2-misol
+
+// let a;
+//
+// a = +prompt("Enter a value for a");
+// if (0 < a) {
+//   alert(Math.trunc(a + 1));
+// } else {
+//   alert(Math.trunc(a - 2));
+// }
+
+// 3-misol
+
+// let a;
+
+// a = +prompt("Enter a value for a");
+// if (0 < a) {
+//   alert(Math.trunc(a + 1));
+// } else {
+//   if (a == 0) {
+//     alert(Math.trunc(10));
+//   } else {
+//     alert(Math.trunc(a - 2));
+//   }
+// }
+
+// // 4-misol
+
+// function main() {
+//   let a;
+
+//   a = +prompt("Enter a value for a");
+//   let b;
+
+//   b = +prompt("Enter a value for b");
+//   let c;
+
+//   c = +prompt("Enter a value for c");
+//   if (a > 0 && b > 0 && c > 0) {
+//     alert(Math.trunc(3));
+//   } else {
+//     if (
+//       (a <= 0 && b > 0 && c > 0) ||
+//       (a > 0 && b <= 0 && c > 0) ||
+//       (a > 0 && b > 0 && c <= 0)
+//     ) {
+//       alert(Math.trunc(2));
+//     } else {
+//       if (
+//         (a > 0 && b <= 0 && c <= 0) ||
+//         (a <= 0 && b > 0 && c <= 0) ||
+//         (a <= 0 && b <= 0 && c > 0)
+//       ) {
+//         alert(Math.trunc(1));
+//       } else {
+//         alert(Math.trunc(0));
+//       }
+//     }
+//   }
+// }
+
+// // 5-misol
+
+// let a;
+
+// a = +prompt("Enter a value for a");
+// let b;
+
+// b = +prompt("Enter a value for b");
+// let c;
+
+// c = +prompt("Enter a value for c");
+// if (a > 0 && b > 0 && c > 0) {
+//   alert("3 positive, 0 minus");
+// } else {
+//   if (
+//     (a <= 0 && b > 0 && c > 0) ||
+//     (a > 0 && b <= 0 && c > 0) ||
+//     (a > 0 && b > 0 && c <= 0)
+//   ) {
+//     alert("2 positive, 1 minus");
+//   } else {
+//     if (
+//       (a > 0 && b <= 0 && c <= 0) ||
+//       (a <= 0 && b > 0 && c <= 0) ||
+//       (a <= 0 && b <= 0 && c > 0)
+//     ) {
+//       alert("1 positive, 2 minus");
+//     } else {
+//       alert("0 positive, 3 minus");
+//     }
+//   }
+// }
+
+// // 6-misol
+
+// let a;
+
+// a = +prompt("Enter a value for a");
+// let b;
+
+// b = +prompt("Enter a value for b");
+// if (a > b) {
+//   alert(Math.trunc(a));
+// } else {
+//   alert(Math.trunc(b));
+// }
+
+// 7-misol
+
+// let a;
+
+// a = +prompt("Enter a value for a");
+// let b;
+
+// b = +prompt("Enter a value for b");
+// if (a < b) {
+//   alert("small number " + Math.trunc(a));
+// } else {
+//   if (a == b) {
+//     alert("teng");
+//   } else {
+//     alert("small number " + Math.trunc(b));
+//   }
+// }
+
+// // 8-misol
+
+// let a;
+
+// a = +prompt("Enter a value for a");
+// let b;
+
+// b = +prompt("Enter a value for b");
+// if (a > b) {
+//   alert(Math.trunc(a));
+//   alert(Math.trunc(b));
+// } else {
+//   if (a == b) {
+//     alert("teng");
+//   } else {
+//     alert(Math.trunc(b));
+//     alert(Math.trunc(a));
+//   }
+// }
